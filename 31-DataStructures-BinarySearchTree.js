3fa0d38b34921f105574d1837a6c57cd860f7d76
Israel Castillo <israelocastillo@gmail.com>
2023-10-21 20:48:02 -0600

    added Data Structures - Binary Search Tree. Info, Operations, usage and logic

---
+++ 31-DataStructures-BinarySearchTree.js
@@ -0,0 +1,14 @@
+//Binary Tree
+//A binary tree is a tree data structure in which each node has at most two children.
+//They are referred as left child and right child.
+
+//Binary Search Tree
+//The value of each left node must be smaller than the parent node.
+//The value of each right node must be greater than the parent node.
+//Each node has at most two children.
+
+//Binary Search Tree Operations:
+//1. Insertion - To add a node to the tree
+//2. Search - To find a node given its value
+//3. DFS(Depth Frist Seach) & BFS(Breadth Fist Search) - To visit all nodes in a tree.
+//4. Deletion - To remove a node given its value.
