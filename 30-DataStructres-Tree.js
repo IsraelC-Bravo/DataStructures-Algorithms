93fe34823830ecb38d5cacffcfd7e2cea4a0d378
Israel Castillo <israelocastillo@gmail.com>
2023-10-21 20:38:10 -0600

    added Data Structures - Tree Overview, info, logic and terminology

---
+++ 30-DataStructres-Tree.js
@@ -0,0 +1,30 @@
+//Tree
+//A tree is a hierarchical data structure that consists of nodes connected by edges.
+//A tree is a non-linear data structure, compared to arrays, linked-lists, stacks and queues which are linear data structures.
+//In a linear data structure, the time required to search is proportional to the size of the data set.
+//Trees however, owing to the non-linar nature allow quicker and easier access to the data.
+//A tree will not contain any loops or cycles.
+
+//Tree Usage
+//1. File systems for directory structure
+//2. A family tree
+//3. An organization tree
+//4. DOM
+//5. Chat bots
+//6. Abstract syntax trees
+
+//Terminology
+//Root Node - the node where the tree originates.
+//Parent Node - any node directly above the selected node.
+//Child - Node - any node under the selected node.
+//Edges - Connection lines or path between nodes.
+//Leaf Node - a node with no child nodes
+//Sibling Node - a node that shares the same parent as another node.
+//Ancestor Node - a node that is higher than the parent node in the same path as the selected node.
+//Path - the line that connects a specific node with the root / parent / ancestor node
+//Distance - pretty much the length of the path or number of edges between one node and another one.
+//Degree of Node - the number of child nodes it has.
+//Degree of Tree - the maximum degree of a node in the tree (root node).
+//Depth - the number of edges from the root to that node (Depth of root node is always 0).
+//Height of Node - the number of edges from the deepest leaf to that node.
+//Height of Tree - the number of egdes from the deepest leaf to the root node.
