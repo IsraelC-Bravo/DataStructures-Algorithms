fc27580656b34aeb6024546ce65d581b243c5499
Israel Castillo <israelocastillo@gmail.com>
2023-10-22 21:16:42 -0600

    added Data Structures - Graph info, types, and usage

---
+++ 32-DataStructures-Graph.js
@@ -0,0 +1,27 @@
+//Graph
+//A graph is a non-linear data structure that consists of a finite number of vertices(nodes) connected by edges
+//Trees are a specific type of graph data structures.
+
+//Types of graphs
+//1. Directed
+//A graph in which the edges have a direction.
+//Edges are usually represented as arrows pointing in the direction the graph can be traversed.
+//A->B->C
+
+//2. Undirected
+//A graph in which the edges are bidirectional.
+//The graph can be traversed in either direction.
+//The absence of arrows tells us the graph is undirected.
+//A-B-C or C-B-A
+
+//More types of graphs.
+//1. Only vortices(nodes) - NO EDGES
+//2. Multiple edges from one node
+//3. Cycles in the graph
+//4. Self Loops on a node
+//5. Disconnected nodes
+//6. A graph may contain weighs on the egdes, representing the cost of traversing the edge.
+
+//Graph Usage
+//Google Maps - Navigation Apps
+//Social Media Apps
